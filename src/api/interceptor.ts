ec7baa3ce78ef559820135d480ee90845fc0ca4d
priyanshu2117 <saxenapriyanshu779@gmail.com>
2026-04-30 22:51:37 +0530

    Added client side request scheduler with synchronization

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,7 +1,14 @@
-import { AxiosHeaders, type InternalAxiosRequestConfig } from "axios";
+import axios, {
+  AxiosError,
+  AxiosHeaders,
+  type AxiosPromise,
+  type AxiosResponse,
+  type InternalAxiosRequestConfig,
+} from "axios";
 import { tokenService } from "../utils/tokenService";
 import { axiosInstance } from "./axiosInstance";
 import { AUTH_EXCLUDE_ROUTES } from "./constants";
+import type { RetryableRequestConfig } from "../types/authTypes";
 
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
@@ -24,6 +31,24 @@ axiosInstance.interceptors.request.use(
   },
 );
 
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (
+  error: AxiosError | null | unknown,
+  token: string | null,
+) => {
+  failedQueue.forEach((promise) => {
+    if (error) {
+      promise.reject(error);
+    } else {
+      promise.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
 axiosInstance.interceptors.response.use(
   (response) => {
     console.log("Response Received:", {
@@ -34,9 +59,57 @@ axiosInstance.interceptors.response.use(
 
     return response;
   },
-  (error) => {
+  async (error: AxiosError) => {
     console.error("API Error:", error.response || error.message);
+    const originalRequest = error.config as RetryableRequestConfig;
 
-    return Promise.reject(error);
+    //if not 401 exit early
+    if (error.response?.status !== 401) return Promise.reject(error);
+
+    //Prevents infinite retry loops
+    if (originalRequest?._retry) {
+      return Promise.reject(error);
+    }
+
+    // if referesh in progress - queue the requests
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        failedQueue.push({
+          promise: (token: string | null) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            resolve(axiosInstance(originalRequest));
+          },
+          reject,
+        });
+      });
+    }
+
+    originalRequest._retry = true;
+    isRefreshing = true;
+    try {
+      const refreshToken = tokenService.getAccessToken();
+
+      const res = await axiosInstance.post("/refresh", {
+        refreshToken,
+      });
+      const newAccessToken = res.data.accessToken;
+      tokenService.setAccessToken(newAccessToken);
+
+      processQueue(null, newAccessToken);
+
+      originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+      return axiosInstance(originalRequest);
+    } catch (err) {
+      processQueue(err, null);
+
+      tokenService.clear();
+
+      window.location.href = "/login";
+
+      return Promise.reject(err);
+    } finally {
+      isRefreshing = false;
+    }
   },
 );
--- src/types/authTypes.ts
+++ src/types/authTypes.ts
@@ -1,4 +1,10 @@
+import type { InternalAxiosRequestConfig } from "axios";
+
 export interface LoginData {
   username: String;
   password: String;
 }
+
+export type RetryableRequestConfig = InternalAxiosRequestConfig & {
+  _retry?: boolean;
+};
